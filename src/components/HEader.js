f1f70f76776139585d99d5fc8ba9a12cfa2c246d
Arunkumar <arunkumar834428@gmail.com>
2021-10-14 09:24:04 +0530

    styling

--- src/components/HEader.js
+++ src/components/HEader.js
@@ -8,7 +8,7 @@ const HEader = ({handleLogOut}) => {
     }
     return (
         <div className="containerh">
-            <h1 className="header" style={{fontFamily:'fantasy'}}>Anime Maze</h1>
+            <h1  style={{fontFamily:'monospace'}}>ANIME MAZE</h1>
             <button onClickCapture={historybutton()}  style={{position:'absolute',right:'0px',color:'red' }}>GoHome</button>
             <button className='logout' onClick={handleLogOut} style={{position:'absolute',fontFamily:'sans-serif' ,top:'20%', right:'0px', backgroundColor:'red', color:'yellow'}}> LOGOUT</button>
         </div>
